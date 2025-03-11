92e456588a8797b0130d55ad8d493b5565b62fc8
myaeba <lim104321@naver.com>
2025-03-11 17:12:27 +0900

    #8-springboot 프로젝트 시작

---
+++ first-board/src/test/java/com/example/firstboard/FirstBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
